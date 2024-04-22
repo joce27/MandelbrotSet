17fc461064f22167ae18e2041fc0e34aa0f00b60
Mina Hedglin <140110601+minahedglin@users.noreply.github.com>
2024-04-21 19:23:43 -0700

    Update ComplexPlane.cpp

--- code/ComplexPlane.cpp
+++ code/ComplexPlane.cpp
@@ -62,21 +62,86 @@ void ComplexPlane::loadText(Text& text)
 
 void ComplexPlane::updateRender()
 {
-
+ if (m_state == State::CALCULATING)
+    {
+        for (int i = 0; i < m_pixel_size.y; ++i)
+        {
+            for (int j = 0; j < m_pixel_size.x; ++j)
+            {
+                m_vArray[j + i * m_pixel_size.x].position = { (float)j, (float)i };
+                Vector2f coords = mapPixelToCoords(Vector2i(j, i));
+                int iterations = countIterations(coords);
+                Uint8 r, g, b;
+                iterationsToRGB(iterations, r, g, b);
+                m_vArray[j + i * m_pixel_size.x].color = Color(r, g, b);
+            }
+        }
+        m_state = State::DISPLAYING;
+    }
 }
 
 // Private member functions
 int ComplexPlane::countIterations(Vector2f coord)
 {
-
+    complex<float> z(0, 0);
+    complex<float> c(coord.x, coord.y);
+    int iterations = 0;
+    while (abs(z) < 2 && iterations < MAX_ITER)
+    {
+        z = z * z + c;
+        iterations++;
+    }
+    return iterations;
 }
 
 void ComplexPlane::iterationsToRGB(size_t count, Uint8& r, Uint8& g, Uint8& b)
 {
-
+    if (count == MAX_ITER)
+    {
+        r = g = b = 0; 
+    }
+    else
+      {
+        size_t region = count / (MAX_ITER / 5);
+        switch (region)
+        {
+            case 0: 
+                r = 128 + count % 128;
+                g = 0;
+                b = 255 - count % 128;
+                break;
+            case 1:
+                r = 0;
+                g = 128 + count % 128;
+                b = 128 + count % 128;
+                break;
+            case 2: 
+                r = 0;
+                g = 255 - count % 128;
+                b = 0;
+                break;
+            case 3: 
+                r = 255 - count % 128;
+                g = 255 - count % 128;
+                b = 0;
+                break;
+            case 4: 
+                r = 255;
+                g = 0;
+                b = count % 128;
+                break;
+        }
+    }
 }
 
 Vector2f ComplexPlane::mapPixelToCoords(Vector2i mousePixel)
 {
+  float minX = m_plane_center.x - m_plane_size.x / 2.0f;
+    float maxX = m_plane_center.x + m_plane_size.x / 2.0f;
+    float minY = m_plane_center.y - m_plane_size.y / 2.0f;
+    float maxY = m_plane_center.y + m_plane_size.y / 2.0f;
+    float x = ((float)mousePixel.x / m_pixel_size.x) * (maxX - minX) + minX;
+    float y = ((float)(m_pixel_size.y - mousePixel.y) / m_pixel_size.y) * (maxY - minY) + minY;
 
+    return Vector2f(x, y); 
 }
